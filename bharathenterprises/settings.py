1c8021e3e2f5b95e61e63a02d184145b0b7a5cda
viyola <viyolamonteiro@gmail.com>
2024-02-14 22:35:52 +0530

    handled cart and checked page for the logged in users

--- bharathenterprises/settings.py
+++ bharathenterprises/settings.py
@@ -132,7 +132,20 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_HOST_USER = 'monteirovsm123@gmail.com'
+EMAIL_HOST_PASSWORD = 'Monteiro@123'
+EMAIL_USE_TLS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#CSRF_FAILURE_VIEW = 'bharathenterprises.views.custom_csrf_failure'
